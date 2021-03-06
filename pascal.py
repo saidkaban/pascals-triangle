e35ab578128b8d90c48e180ec93aceefbaf4e2c5
saidkaban <saidkaban@yahoo.com>
2021-03-07 00:18:12 +0300

    Success

---
+++ pascal.py
@@ -0,0 +1,19 @@
+def generate(numRows):
+    result = [[1]]
+    if numRows == 1:
+        return result
+    elif numRows == 2:
+        result.append([1, 1])
+        return result
+    else:
+        result.append([1, 1])
+        for n in range(3, numRows + 1):
+            row_result = [1]
+            for i in range(1, n - 1):
+                a = sum(result[n - 2][(i-1):(i+1)])
+                row_result.append(a)
+            row_result.append(1)
+            result.append(row_result)
+        return result
+        
+print(generate(5))
